e133927a34b58f5704ade194588a8917dcdef129
Gamal sayed <gamalsayedccss@gmail.com>
2025-03-13 19:26:39 +0200

    adding swiper projects section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,18 +1,23 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
+
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 import styles from "./projects.module.css";
 import { useState } from "react";
 import maindata from "./items.json";
-
+import bg from "./imgs/bg.png";
 export default function Projects() {
   let [currentActive, setCurrentActive] = useState("all");
   let [mainData, setMainData] = useState(maindata);
   return (
     <section id="Portfolio" className={`${styles.portfolio}`}>
+      <img src={bg} alt="bg not found" className={styles.bg} />
       <div className={`container ${styles.projects_container}`}>
         <div className={`${styles.buttons}`}>
           <button
@@ -52,44 +57,37 @@ export default function Projects() {
         </div>
 
         <div className={`${styles.projects}`}>
-          {mainData.map((item) => {
-            return (
-              <a
-                className={`${styles.item} wow fadeInUp`}
-                target="_blank"
-                rel="noreferrer"
-                href={item.link}
-              >
-                {item.imgs ? (
-                  <>
-                    <Swiper
-                      className="mySwiper"
-                      autoplay={{
-                        delay: 2500,
-                        disableOnInteraction: false,
-                      }}
-                      modules={[Autoplay]}
-                    >
-                      {item.imgs.map((img, index) => (
-                        <SwiperSlide key={index}>
-                          {" "}
-                          <img src={img} alt="img not found" />
-                        </SwiperSlide>
-                      ))}
-                    </Swiper>
-                  </>
-                ) : (
-                  <>
+          <Swiper
+            className={styles.swiper}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            modules={[Autoplay, Pagination, Navigation]}
+            pagination={{
+              clickable: true,
+            }}
+          >
+            {mainData.map((item) => {
+              return (
+                <SwiperSlide className={styles.slide} key={item.id}>
+                  <a href="" className={styles.img}>
                     {" "}
-                    <img src={item.imgUrl} alt="img not found" />
-                  </>
-                )}
-
-                <h3>{item.name}</h3>
-                <p>{item.description}</p>
-              </a>
-            );
-          })}
+                    <img src={item.imgUrl} alt="" />
+                  </a>
+                  <a
+                    className={`${styles.item} wow fadeInUp`}
+                    target="_blank"
+                    rel="noreferrer"
+                    href={item.link}
+                  >
+                    <h3>{item.name}</h3>
+                    <p>{item.description}</p>
+                  </a>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
         </div>
       </div>
     </section>
